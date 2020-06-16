89ce496a364f64f34619651a33c1449c9f18fc10
Holly <57748227+Holly-hyojin@users.noreply.github.com>
2020-06-16 16:35:54 +0100

    JS: arrow up

--- main.js
+++ main.js
@@ -52,6 +52,21 @@ document.addEventListener('scroll', () => {
 //   fadeHome.style.opacity = '0.5';
 // });
 
+// Show arrow up button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+// Handle click arrow up button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
